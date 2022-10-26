3e4eb35236e5e1a0d8f8342c6f17e4bece6c50db
aokihayaaaato <aokihayato.sales@gmail.com>
2022-10-26 10:20:22 +0900

    props

--- resources/views/components/tests/card.blade.php
+++ resources/views/components/tests/card.blade.php
@@ -1,3 +1,12 @@
+<!-- componentの属性に対して初期値を設定 -->
+<!-- コントローラー側から値が渡ってきてないときは初期値が表示される  -->
+<!-- 初期値なしの場合、キー名だけでValueはなしでOK -->
+@props([
+    'title',
+    'message' => '初期値です。',
+    'content' => '本文初期値です'
+])
+
 <div class="border-2 shadow-md w-1/4 p-2">
     <div>{{ $title }}</div>
     <div>画像</div>
--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -2,4 +2,5 @@
     <x-slot name="header">ヘッダー1</x-slot>
     コンポーネントテスト1
     <x-tests.card title="タイトル1" content="本文1" :message="$message" /><!-- 変数の値を渡す場合は【:属性名="変数"】で渡さないと変数の値が展開されず変数名がそのまま表示される -->
+    <x-tests.card title="タイトル2" />
 </x-tests.app>
