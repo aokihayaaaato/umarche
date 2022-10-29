9563cf508c88326c59676681bb055b6c68496bcd
aokihayaaaato <aokihayato.sales@gmail.com>
2022-10-30 01:27:29 +0900

    edit, update

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -8,6 +8,7 @@ use App\Models\Owner;   // Eloquent
 use Illuminate\Support\Facades\DB;  // QueryBuilder
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rule; // update処理の際にemailのuniqueバリデーションは避けたい為
 
 class OwnersController extends Controller
 {
@@ -35,7 +36,7 @@ class OwnersController extends Controller
 
         // dd($e_all, $q_get, $q_first, $c_test);
 
-        $owners = Owner::select('name', 'email', 'created_at')->get();
+        $owners = Owner::select('id', 'name', 'email', 'created_at')->get();
 
         return view('admin.owners.index', compact('owners'));
     }
@@ -95,7 +96,10 @@ class OwnersController extends Controller
      */
     public function edit($id)
     {
-        //
+        $owner = Owner::findOrFail($id);
+        // dd($owner);
+
+        return view('admin.owners.edit', compact('owner'));
     }
 
     /**
@@ -107,7 +111,21 @@ class OwnersController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', Rule::unique('owners')->ignore($id)],
+            'password' => ['required', 'confirmed', 'min:8'],
+        ]);
+
+        $owner = Owner::findOrFail($id);
+        $owner->name = $request->name;
+        $owner->email = $request->email;
+        $owner->password = Hash::make($request->password);
+        $owner->save();
+
+        return redirect()
+        ->route('admin.owners.index')
+        ->with('message', 'オーナー情報を更新しました。');
     }
 
     /**
