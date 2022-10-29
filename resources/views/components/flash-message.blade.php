b2919c5d752525eca9caa8d6ca23cf2f5ada56c1
aokihayaaaato <aokihayato.sales@gmail.com>
2022-10-29 22:15:10 +0900

    Store処理
    - セッションフラッシュメッセージ追加（JS機能は未実装）

---
+++ resources/views/components/flash-message.blade.php
@@ -0,0 +1,19 @@
+{{--  【status】の初期値はinfoとする  --}}
+@props([
+  'status' => 'info'
+])
+
+@php
+  if($status === 'info') {
+    $bgColor = 'bg-blue-300';
+  }
+  if($status === 'error') {
+    $bgColor = 'bg-red-500';
+  }
+@endphp
+
+@if (session('message'))
+  <div class="{{ $bgColor }} w-1/2 mx-auto p-2 text-white">
+    {{ session('message') }}
+  </div>
+@endif
